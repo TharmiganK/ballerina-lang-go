c1f97eda914cc70c012a6c85f5713d0778be890c
Heshan Padamsiri <hpheshan@gmail.com>
2026-07-14 11:15:03 +0530

    fix: keep object method helpers private

--- semantics/symbol_resolver.go
+++ semantics/symbol_resolver.go
@@ -1323,7 +1323,7 @@ func visitInnerSymbolResolver[T symbolResolver](resolver T, node ast.BLangNode) 
 		resolveFunctionTypeSymbols(resolver, &n.BLangFunctionType)
 		if n.Symbol().IsEmpty() {
 			space := resolver.GetScope().MainSpace()
-			index := space.AppendSymbol(model.NewFunctionSymbol(n.Name(), model.TypedFunctionSignature{}, n.IsPublic(), n.GetPosition()))
+			index := space.AppendSymbol(model.NewFunctionSymbol(n.Name(), model.TypedFunctionSignature{}, false, n.GetPosition()))
 			n.SetSymbol(space.RefAt(index))
 		}
 		associateFunctionSignatureRef(resolver.GetCtx(), n.Symbol(), n.SignatureRef(), n.GetPosition())

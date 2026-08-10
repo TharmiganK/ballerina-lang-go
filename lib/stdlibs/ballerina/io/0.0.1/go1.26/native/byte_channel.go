ballerina-nutcracker/ballerina

feat(io): return read-only byte arrays
Promote io:Block to readonly & byte[] and change fileReadBytes and
ReadableByteChannel.readAll to return readonly & byte[], aligning the
public contract with jBallerina now that the runtime supports
readonly & byte[] intersections. ReadableByteChannel.read keeps its
mutable byte[] return, as in jBallerina.

Native byte-array construction uses a CELL_MUT_NONE list definition
(the atom-backed equivalent of the readonly & byte[] intersection) so
runtime type tests against readonly targets pass, and marks the values
read-only so mutation panics as jBallerina does. Drop the now-obsolete
divergence notes from the READMEs and subset docs, and add a corpus
test pinning the read-only contract.

lib/stdlibs/ballerina/io/0.0.1/go1.26/native/byte_channel.go
@@ -80,8 +80,10 @@ func byteChannelEofError() values.BalValue {
 }
 
 type byteChannelTypes struct {
-	byteArrTy   semtypes.SemType
-	byteArrAtom *semtypes.ListAtomicType
+	byteArrTy     semtypes.SemType
+	byteArrAtom   *semtypes.ListAtomicType
+	roByteArrTy   semtypes.SemType
+	roByteArrAtom *semtypes.ListAtomicType
 
 	blockStreamTy  semtypes.SemType
 	blockRecordTy  semtypes.SemType
@@ -97,12 +99,17 @@ func initByteChannelModule(rt *runtime.Runtime) {
 		byteArrTy: bld.DefineListTypeWrappedWithEnvSemType(env, semtypes.BYTE),
 	}
 	types.byteArrAtom = semtypes.ToListAtomicType(typCtx, types.byteArrTy)
+	// io:Block is `readonly & byte[]`; a CELL_MUT_NONE list definition is the
+	// atom-backed equivalent of that intersection.
+	robld := semtypes.NewListDefinition()
+	types.roByteArrTy = robld.DefineListTypeWrappedWithEnvSemTypeCellMutability(env, semtypes.BYTE, semtypes.CellMutability_CELL_MUT_NONE)
+	types.roByteArrAtom = semtypes.ToListAtomicType(typCtx, types.roByteArrTy)
 
 	streamCompletionTy := semtypes.Union(semtypes.ERROR, semtypes.NIL)
 	bsd := semtypes.NewStreamDefinition()
-	types.blockRecordTy = closedNextRecordType(env, types.byteArrTy)
+	types.blockRecordTy = closedNextRecordType(env, types.roByteArrTy)
 	types.blockRecordAtm = semtypes.ToMappingAtomicType(typCtx, types.blockRecordTy)
-	types.blockStreamTy = bsd.Define(env, types.byteArrTy, streamCompletionTy)
+	types.blockStreamTy = bsd.Define(env, types.roByteArrTy, streamCompletionTy)
 
 	runtime.RegisterExternFunction(rt, orgName, moduleName, "ReadableByteChannel.attachFile",
 		func(_ *extern.Context, args []values.BalValue) (values.BalValue, error) {
@@ -168,7 +175,7 @@ func initByteChannelModule(rt *runtime.Runtime) {
 			if err != nil {
 				return fileIOError("error occurred while reading bytes from the channel. " + err.Error()), nil
 			}
-			return values.NewList(types.byteArrTy, types.byteArrAtom, false, nil, 0, bytesToItems(data)), nil
+			return values.NewList(types.roByteArrTy, types.roByteArrAtom, true, nil, 0, bytesToItems(data)), nil
 		})
 
 	runtime.RegisterExternFunction(rt, orgName, moduleName, "ReadableByteChannel.blockStream",
@@ -191,7 +198,7 @@ func initByteChannelModule(rt *runtime.Runtime) {
 				buf := make([]byte, size)
 				n, err := reader.Read(buf)
 				if n > 0 {
-					block := values.NewList(types.byteArrTy, types.byteArrAtom, false, nil, 0, bytesToItems(buf[:n]))
+					block := values.NewList(types.roByteArrTy, types.roByteArrAtom, true, nil, 0, bytesToItems(buf[:n]))
 					return values.NewMap(types.blockRecordTy, types.blockRecordAtm, false,
 						[]values.MapEntry{{Key: "value", Value: block}})
 				}

lib/stdlibs/ballerina/io/0.0.1/go1.26/native/file_io.go
@@ -30,11 +30,13 @@ import (
 )
 
 type fileIOTypes struct {
-	strArrTy    semtypes.SemType
-	byteArrTy   semtypes.SemType
-	byteArrAtom *semtypes.ListAtomicType
-	jsonListTy  semtypes.SemType
-	jsonMapTy   semtypes.SemType
+	strArrTy      semtypes.SemType
+	byteArrTy     semtypes.SemType
+	byteArrAtom   *semtypes.ListAtomicType
+	roByteArrTy   semtypes.SemType
+	roByteArrAtom *semtypes.ListAtomicType
+	jsonListTy    semtypes.SemType
+	jsonMapTy     semtypes.SemType
 
 	lineStreamTy   semtypes.SemType
 	lineRecordTy   semtypes.SemType
@@ -82,6 +84,11 @@ func initFileIOModule(rt *runtime.Runtime) {
 	}
 
 	types.byteArrAtom = semtypes.ToListAtomicType(typCtx, types.byteArrTy)
+	// io:Block is `readonly & byte[]`; a CELL_MUT_NONE list definition is the
+	// atom-backed equivalent of that intersection.
+	robld := semtypes.NewListDefinition()
+	types.roByteArrTy = robld.DefineListTypeWrappedWithEnvSemTypeCellMutability(env, semtypes.BYTE, semtypes.CellMutability_CELL_MUT_NONE)
+	types.roByteArrAtom = semtypes.ToListAtomicType(typCtx, types.roByteArrTy)
 
 	streamCompletionTy := semtypes.Union(semtypes.ERROR, semtypes.NIL)
 
@@ -91,9 +98,9 @@ func initFileIOModule(rt *runtime.Runtime) {
 	types.lineStreamTy = lsd.Define(env, semtypes.STRING, streamCompletionTy)
 
 	bsd := semtypes.NewStreamDefinition()
-	types.blockRecordTy = closedNextRecordType(env, types.byteArrTy)
+	types.blockRecordTy = closedNextRecordType(env, types.roByteArrTy)
 	types.blockRecordAtom = semtypes.ToMappingAtomicType(typCtx, types.blockRecordTy)
-	types.blockStreamTy = bsd.Define(env, types.byteArrTy, streamCompletionTy)
+	types.blockStreamTy = bsd.Define(env, types.roByteArrTy, streamCompletionTy)
 
 	runtime.RegisterExternFunction(rt, orgName, moduleName, "externFileReadString",
 		func(_ *extern.Context, args []values.BalValue) (values.BalValue, error) {
@@ -133,7 +140,7 @@ func initFileIOModule(rt *runtime.Runtime) {
 			for i, b := range data {
 				items[i] = int64(b)
 			}
-			return values.NewList(types.byteArrTy, semtypes.ToListAtomicType(ctx.TypeCtx, types.byteArrTy), false, nil, 0, items), nil
+			return values.NewList(types.roByteArrTy, types.roByteArrAtom, true, nil, 0, items), nil
 		})
 
 	runtime.RegisterExternFunction(rt, orgName, moduleName, "externFileReadJson",

TharmiganK/ballerina-lang-go

lib/stdlibs/ballerina/io/0.0.1/go1.26/native/byte_channel.go
@@ -80,8 +80,10 @@ func byteChannelEofError() values.BalValue {
 }
 
 type byteChannelTypes struct {
-	byteArrTy   semtypes.SemType
-	byteArrAtom *semtypes.ListAtomicType
+	byteArrTy     semtypes.SemType
+	byteArrAtom   *semtypes.ListAtomicType
+	roByteArrTy   semtypes.SemType
+	roByteArrAtom *semtypes.ListAtomicType
 
 	blockStreamTy  semtypes.SemType
 	blockRecordTy  semtypes.SemType
@@ -97,12 +99,17 @@ func initByteChannelModule(rt *runtime.Runtime) {
 		byteArrTy: bld.DefineListTypeWrappedWithEnvSemType(env, semtypes.BYTE),
 	}
 	types.byteArrAtom = semtypes.ToListAtomicType(env, types.byteArrTy)
+	// io:Block is `readonly & byte[]`; a CELL_MUT_NONE list definition is the
+	// atom-backed equivalent of that intersection.
+	robld := semtypes.NewListDefinition()
+	types.roByteArrTy = robld.DefineListTypeWrappedWithEnvSemTypeCellMutability(env, semtypes.BYTE, semtypes.CellMutability_CELL_MUT_NONE)
+	types.roByteArrAtom = semtypes.ToListAtomicType(env, types.roByteArrTy)
 
 	streamCompletionTy := semtypes.Union(semtypes.ERROR, semtypes.NIL)
 	bsd := semtypes.NewStreamDefinition()
-	types.blockRecordTy = closedNextRecordType(env, types.byteArrTy)
+	types.blockRecordTy = closedNextRecordType(env, types.roByteArrTy)
 	types.blockRecordAtm = semtypes.ToMappingAtomicType(typCtx, types.blockRecordTy)
-	types.blockStreamTy = bsd.Define(env, types.byteArrTy, streamCompletionTy)
+	types.blockStreamTy = bsd.Define(env, types.roByteArrTy, streamCompletionTy)
 
 	runtime.RegisterExternFunction(rt, orgName, moduleName, "ReadableByteChannel.attachFile",
 		func(_ *extern.Context, args []values.BalValue) (values.BalValue, error) {
@@ -168,7 +175,7 @@ func initByteChannelModule(rt *runtime.Runtime) {
 			if err != nil {
 				return fileIOError("error occurred while reading bytes from the channel. " + err.Error()), nil
 			}
-			return values.NewList(types.byteArrTy, types.byteArrAtom, false, nil, 0, bytesToItems(data)), nil
+			return values.NewList(types.roByteArrTy, types.roByteArrAtom, true, nil, 0, bytesToItems(data)), nil
 		})
 
 	runtime.RegisterExternFunction(rt, orgName, moduleName, "ReadableByteChannel.blockStream",
@@ -191,7 +198,7 @@ func initByteChannelModule(rt *runtime.Runtime) {
 				buf := make([]byte, size)
 				n, err := reader.Read(buf)
 				if n > 0 {
-					block := values.NewList(types.byteArrTy, types.byteArrAtom, false, nil, 0, bytesToItems(buf[:n]))
+					block := values.NewList(types.roByteArrTy, types.roByteArrAtom, true, nil, 0, bytesToItems(buf[:n]))
 					return values.NewMap(types.blockRecordTy, types.blockRecordAtm, false,
 						[]values.MapEntry{{Key: "value", Value: block}})
 				}

lib/stdlibs/ballerina/io/0.0.1/go1.26/native/file_io.go
@@ -30,11 +30,13 @@ import (
 )
 
 type fileIOTypes struct {
-	strArrTy    semtypes.SemType
-	byteArrTy   semtypes.SemType
-	byteArrAtom *semtypes.ListAtomicType
-	jsonListTy  semtypes.SemType
-	jsonMapTy   semtypes.SemType
+	strArrTy      semtypes.SemType
+	byteArrTy     semtypes.SemType
+	byteArrAtom   *semtypes.ListAtomicType
+	roByteArrTy   semtypes.SemType
+	roByteArrAtom *semtypes.ListAtomicType
+	jsonListTy    semtypes.SemType
+	jsonMapTy     semtypes.SemType
 
 	lineStreamTy   semtypes.SemType
 	lineRecordTy   semtypes.SemType
@@ -82,6 +84,11 @@ func initFileIOModule(rt *runtime.Runtime) {
 	}
 
 	types.byteArrAtom = semtypes.ToListAtomicType(env, types.byteArrTy)
+	// io:Block is `readonly & byte[]`; a CELL_MUT_NONE list definition is the
+	// atom-backed equivalent of that intersection.
+	robld := semtypes.NewListDefinition()
+	types.roByteArrTy = robld.DefineListTypeWrappedWithEnvSemTypeCellMutability(env, semtypes.BYTE, semtypes.CellMutability_CELL_MUT_NONE)
+	types.roByteArrAtom = semtypes.ToListAtomicType(env, types.roByteArrTy)
 
 	streamCompletionTy := semtypes.Union(semtypes.ERROR, semtypes.NIL)
 
@@ -91,9 +98,9 @@ func initFileIOModule(rt *runtime.Runtime) {
 	types.lineStreamTy = lsd.Define(env, semtypes.STRING, streamCompletionTy)
 
 	bsd := semtypes.NewStreamDefinition()
-	types.blockRecordTy = closedNextRecordType(env, types.byteArrTy)
+	types.blockRecordTy = closedNextRecordType(env, types.roByteArrTy)
 	types.blockRecordAtom = semtypes.ToMappingAtomicType(typCtx, types.blockRecordTy)
-	types.blockStreamTy = bsd.Define(env, types.byteArrTy, streamCompletionTy)
+	types.blockStreamTy = bsd.Define(env, types.roByteArrTy, streamCompletionTy)
 
 	runtime.RegisterExternFunction(rt, orgName, moduleName, "externFileReadString",
 		func(_ *extern.Context, args []values.BalValue) (values.BalValue, error) {
@@ -133,7 +140,7 @@ func initFileIOModule(rt *runtime.Runtime) {
 			for i, b := range data {
 				items[i] = int64(b)
 			}
-			return values.NewList(types.byteArrTy, semtypes.ToListAtomicType(ctx.TypeEnv(), types.byteArrTy), false, nil, 0, items), nil
+			return values.NewList(types.roByteArrTy, types.roByteArrAtom, true, nil, 0, items), nil
 		})
 
 	runtime.RegisterExternFunction(rt, orgName, moduleName, "externFileReadJson",
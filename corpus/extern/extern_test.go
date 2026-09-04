ballerina-nutcracker/ballerina

refactor(nodebuilder): replace panics with diagnostics

corpus/extern/extern_test.go
@@ -778,7 +778,8 @@ func compileSingleFileModule(
 		defaultOrg,
 	)
 	assertNoDiagnostics(t, cx, "ResolveSymbols")
-	pkg := nodebuilder.ToPackageFromCompilationUnits(compilationUnits)
+	pkg := nodebuilder.ToPackageFromCompilationUnitsWithContext(cx, compilationUnits)
+	assertNoDiagnostics(t, cx, "ToPackageFromCompilationUnits")
 	pkg.PackageID = pkgID
 	pkg.Scope = pkgScope
 	pkg.Imports = nil

corpus/integration_test.go
@@ -651,7 +651,10 @@ func compileModuleFromSource(env *context.CompilerEnvironment, project projects.
 	if cx.HasDiagnostics() {
 		return nil, fmt.Errorf("symbol resolution failed")
 	}
-	pkg := nodebuilder.ToPackageFromCompilationUnits(syntaxTrees)
+	pkg := nodebuilder.ToPackageFromCompilationUnitsWithContext(cx, syntaxTrees)
+	if cx.HasDiagnostics() {
+		return nil, fmt.Errorf("package assembly failed")
+	}
 	pkg.Imports = nil
 	pkg.PackageID = pkgID
 	pkg.Scope = pkgScope

nodebuilder/mod.go
@@ -22,6 +22,7 @@ import (
 	"github.com/ballerina-nutcracker/ballerina/ast"
 	"github.com/ballerina-nutcracker/ballerina/context"
 	"github.com/ballerina-nutcracker/ballerina/st"
+	"github.com/ballerina-nutcracker/ballerina/tools/diagnostics"
 )
 
 func GetCompilationUnit(cx *context.CompilerContext, syntaxTree *st.SyntaxTree) *ast.BLangCompilationUnit {
@@ -38,20 +39,29 @@ func GetRecoveredCompilationUnit(cx *context.CompilerContext, syntaxTree *st.Syn
 }
 
 func ToPackageFromCompilationUnits(compilationUnits []*ast.BLangCompilationUnit) *ast.BLangPackage {
+	return toPackageFromCompilationUnits(nil, compilationUnits)
+}
+
+func ToPackageFromCompilationUnitsWithContext(cx *context.CompilerContext, compilationUnits []*ast.BLangCompilationUnit) *ast.BLangPackage {
+	return toPackageFromCompilationUnits(cx, compilationUnits)
+}
+
+func toPackageFromCompilationUnits(cx *context.CompilerContext, compilationUnits []*ast.BLangCompilationUnit) *ast.BLangPackage {
 	pkg := ast.NewBLangPackage()
 	for _, compilationUnit := range compilationUnits {
 		packageID := compilationUnit.GetPackageID()
 		if pkg.PackageID == nil {
 			pkg.PackageID = packageID
 		} else if packageID != nil && pkg.PackageID != packageID {
-			panic("compilation units have different package IDs")
+			reportPackageAssemblyError(cx, "compilation units have different package IDs", compilationUnit.GetPosition())
+			return pkg
 		}
-		addCompilationUnitNodesToPackage(pkg, compilationUnit)
+		addCompilationUnitNodesToPackage(cx, pkg, compilationUnit)
 	}
 	return pkg
 }
 
-func addCompilationUnitNodesToPackage(pkg *ast.BLangPackage, compilationUnit *ast.BLangCompilationUnit) {
+func addCompilationUnitNodesToPackage(cx *context.CompilerContext, pkg *ast.BLangPackage, compilationUnit *ast.BLangCompilationUnit) {
 	for _, node := range compilationUnit.GetTopLevelNodes() {
 		switch node := node.(type) {
 		case *ast.BLangImportPackage:
@@ -79,7 +89,17 @@ func addCompilationUnitNodesToPackage(pkg *ast.BLangPackage, compilationUnit *as
 		case *ast.BLangClassDefinition:
 			pkg.ClassDefinitions = append(pkg.ClassDefinitions, node)
 		default:
-			panic(fmt.Sprintf("unexpected top-level node type: %T", node))
+			pos := compilationUnit.GetPosition()
+			if node != nil {
+				pos = node.GetPosition()
+			}
+			reportPackageAssemblyError(cx, fmt.Sprintf("unexpected top-level node type: %T", node), pos)
 		}
+	}
+}
+
+func reportPackageAssemblyError(cx *context.CompilerContext, message string, pos diagnostics.Location) {
+	if cx != nil {
+		cx.InternalError(message, pos)
 	}
 }

test_util/langlib/langlib.go
@@ -274,7 +274,10 @@ func compileBundledLib(cx *context.CompilerContext, cache map[string]model.Expor
 		make(map[semantics.PackageIdentifier]model.ExportedSymbolSpace),
 		lib.org,
 	)
-	pkg := nodebuilder.ToPackageFromCompilationUnits(compilationUnits)
+	pkg := nodebuilder.ToPackageFromCompilationUnitsWithContext(cx, compilationUnits)
+	if cx.HasErrors() {
+		return model.ExportedSymbolSpace{}, fmt.Errorf("langlib: package assembly failed for %s", lib.implicitID)
+	}
 	pkg.PackageID = pkgID
 	pkg.Scope = pkgScope
 	pkg.Imports = nil

test_util/testphases/phases.go
@@ -132,7 +132,6 @@ func loadBuiltinPublicSymbols(env *context.CompilerEnvironment) map[semantics.Pa
 		cu.SetPackageID(pkgID)
 		compilationUnits := []*ast.BLangCompilationUnit{cu}
 
-		// Pass accumulated stdlib symbols so packages that import other stdlibs (e.g. os→io, crypto→time) resolve correctly.
 		pkgScope, exported, importedSymbols := semantics.ResolveSymbols(
 			cx,
 			*pkgID,
@@ -144,7 +143,10 @@ func loadBuiltinPublicSymbols(env *context.CompilerEnvironment) map[semantics.Pa
 		if cx.HasErrors() {
 			continue
 		}
-		pkg := nodebuilder.ToPackageFromCompilationUnits(compilationUnits)
+		pkg := nodebuilder.ToPackageFromCompilationUnitsWithContext(cx, compilationUnits)
+		if cx.HasErrors() {
+			continue
+		}
 		pkg.PackageID = pkgID
 		pkg.Scope = pkgScope
 		pkg.Imports = nil
@@ -202,7 +204,7 @@ func RunPipelineWithContent(env *context.CompilerEnvironment, cx *context.Compil
 		return nil, fmt.Errorf("AST generation failed: compilation unit is nil")
 	}
 	if phase == PhaseAST {
-		result.Package = nodebuilder.ToPackageFromCompilationUnits([]*ast.BLangCompilationUnit{result.CompilationUnit})
+		result.Package = nodebuilder.ToPackageFromCompilationUnitsWithContext(cx, []*ast.BLangCompilationUnit{result.CompilationUnit})
 		return result, nil
 	}
 
@@ -225,7 +227,7 @@ func RunPipelineWithContent(env *context.CompilerEnvironment, cx *context.Compil
 		langlibs.PublicSymbols,
 		"",
 	)
-	result.Package = nodebuilder.ToPackageFromCompilationUnits(compilationUnits)
+	result.Package = nodebuilder.ToPackageFromCompilationUnitsWithContext(cx, compilationUnits)
 	result.Package.PackageID = pkgID
 	result.Package.Scope = pkgScope
 	if phase == PhaseSymbolResolution || cx.HasDiagnostics() {

TharmiganK/ballerina-lang-go

corpus/extern/extern_test.go
@@ -788,7 +788,8 @@ func compileSingleFileModule(
 		defaultOrg,
 	)
 	assertNoDiagnostics(t, cx, "ResolveSymbols")
-	pkg := nodebuilder.ToPackageFromCompilationUnits(compilationUnits)
+	pkg := nodebuilder.ToPackageFromCompilationUnits(cx, compilationUnits)
+	assertNoDiagnostics(t, cx, "ToPackageFromCompilationUnits")
 	pkg.PackageID = pkgID
 	pkg.Scope = pkgScope
 	pkg.Imports = nil

corpus/integration_test.go
@@ -668,7 +668,10 @@ func compileModuleFromSource(env *context.CompilerEnvironment, project projects.
 	if cx.HasDiagnostics() {
 		return nil, fmt.Errorf("symbol resolution failed")
 	}
-	pkg := nodebuilder.ToPackageFromCompilationUnits(syntaxTrees)
+	pkg := nodebuilder.ToPackageFromCompilationUnits(cx, syntaxTrees)
+	if cx.HasDiagnostics() {
+		return nil, fmt.Errorf("package assembly failed")
+	}
 	pkg.Imports = nil
 	pkg.PackageID = pkgID
 	pkg.Scope = pkgScope

nodebuilder/mod.go
@@ -22,6 +22,7 @@ import (
 	"github.com/ballerina-nutcracker/ballerina/ast"
 	"github.com/ballerina-nutcracker/ballerina/context"
 	"github.com/ballerina-nutcracker/ballerina/st"
+	"github.com/ballerina-nutcracker/ballerina/tools/diagnostics"
 )
 
 func GetCompilationUnit(cx *context.CompilerContext, syntaxTree *st.SyntaxTree) *ast.BLangCompilationUnit {
@@ -37,21 +38,22 @@ func GetRecoveredCompilationUnit(cx *context.CompilerContext, syntaxTree *st.Syn
 	return compilationUnit.(*ast.BLangCompilationUnit)
 }
 
-func ToPackageFromCompilationUnits(compilationUnits []*ast.BLangCompilationUnit) *ast.BLangPackage {
+func ToPackageFromCompilationUnits(cx *context.CompilerContext, compilationUnits []*ast.BLangCompilationUnit) *ast.BLangPackage {
 	pkg := ast.NewBLangPackage()
 	for _, compilationUnit := range compilationUnits {
 		packageID := compilationUnit.GetPackageID()
 		if pkg.PackageID == nil {
 			pkg.PackageID = packageID
 		} else if packageID != nil && pkg.PackageID != packageID {
-			panic("compilation units have different package IDs")
+			reportPackageAssemblyError(cx, "compilation units have different package IDs", compilationUnit.GetPosition())
+			return pkg
 		}
-		addCompilationUnitNodesToPackage(pkg, compilationUnit)
+		addCompilationUnitNodesToPackage(cx, pkg, compilationUnit)
 	}
 	return pkg
 }
 
-func addCompilationUnitNodesToPackage(pkg *ast.BLangPackage, compilationUnit *ast.BLangCompilationUnit) {
+func addCompilationUnitNodesToPackage(cx *context.CompilerContext, pkg *ast.BLangPackage, compilationUnit *ast.BLangCompilationUnit) {
 	for _, node := range compilationUnit.GetTopLevelNodes() {
 		switch node := node.(type) {
 		case *ast.BLangImportPackage:
@@ -79,7 +81,16 @@ func addCompilationUnitNodesToPackage(pkg *ast.BLangPackage, compilationUnit *as
 		case *ast.BLangClassDefinition:
 			pkg.ClassDefinitions = append(pkg.ClassDefinitions, node)
 		default:
-			panic(fmt.Sprintf("unexpected top-level node type: %T", node))
+			pos := compilationUnit.GetPosition()
+			if node != nil {
+				pos = node.GetPosition()
+			}
+			reportPackageAssemblyError(cx, fmt.Sprintf("unexpected top-level node type: %T", node), pos)
+			return
 		}
 	}
+}
+
+func reportPackageAssemblyError(cx *context.CompilerContext, message string, pos diagnostics.Location) {
+	cx.InternalError(message, pos)
 }

test_util/langlib/langlib.go
@@ -251,7 +251,10 @@ func compileBundledLib(cx *context.CompilerContext, cache map[string]model.Expor
 		make(map[semantics.PackageIdentifier]model.ExportedSymbolSpace),
 		lib.org,
 	)
-	pkg := nodebuilder.ToPackageFromCompilationUnits(compilationUnits)
+	pkg := nodebuilder.ToPackageFromCompilationUnits(cx, compilationUnits)
+	if cx.HasErrors() {
+		return model.ExportedSymbolSpace{}, fmt.Errorf("langlib: package assembly failed for %s", lib.implicitID)
+	}
 	pkg.PackageID = pkgID
 	pkg.Scope = pkgScope
 	pkg.Imports = nil

test_util/testphases/phases.go
@@ -132,7 +132,6 @@ func loadBuiltinPublicSymbols(env *context.CompilerEnvironment) map[semantics.Pa
 		cu.SetPackageID(pkgID)
 		compilationUnits := []*ast.BLangCompilationUnit{cu}
 
-		// Pass accumulated stdlib symbols so packages that import other stdlibs (e.g. os→io, crypto→time) resolve correctly.
 		pkgScope, exported, importedSymbols := semantics.ResolveSymbols(
 			cx,
 			*pkgID,
@@ -144,7 +143,10 @@ func loadBuiltinPublicSymbols(env *context.CompilerEnvironment) map[semantics.Pa
 		if cx.HasErrors() {
 			continue
 		}
-		pkg := nodebuilder.ToPackageFromCompilationUnits(compilationUnits)
+		pkg := nodebuilder.ToPackageFromCompilationUnits(cx, compilationUnits)
+		if cx.HasErrors() {
+			continue
+		}
 		pkg.PackageID = pkgID
 		pkg.Scope = pkgScope
 		pkg.Imports = nil
@@ -202,7 +204,7 @@ func RunPipelineWithContent(env *context.CompilerEnvironment, cx *context.Compil
 		return nil, fmt.Errorf("AST generation failed: compilation unit is nil")
 	}
 	if phase == PhaseAST {
-		result.Package = nodebuilder.ToPackageFromCompilationUnits([]*ast.BLangCompilationUnit{result.CompilationUnit})
+		result.Package = nodebuilder.ToPackageFromCompilationUnits(cx, []*ast.BLangCompilationUnit{result.CompilationUnit})
 		return result, nil
 	}
 
@@ -225,7 +227,7 @@ func RunPipelineWithContent(env *context.CompilerEnvironment, cx *context.Compil
 		langlibs.PublicSymbols,
 		"",
 	)
-	result.Package = nodebuilder.ToPackageFromCompilationUnits(compilationUnits)
+	result.Package = nodebuilder.ToPackageFromCompilationUnits(cx, compilationUnits)
 	result.Package.PackageID = pkgID
 	result.Package.Scope = pkgScope
 	if phase == PhaseSymbolResolution || cx.HasDiagnostics() {